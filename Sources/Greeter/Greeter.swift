9c94946230c8bc7b3b9d3cb93530cff2f857adb8
Marwan Sulaiman <marwan.sameer@gmail.com>
2020-08-11 16:24:21 -0400

    add public

--- Sources/Greeter/Greeter.swift
+++ Sources/Greeter/Greeter.swift
@@ -2,10 +2,10 @@ struct Greeter {
     var text = "Hello, World!"
 }
 
-func SayHello() -> String {
+public func SayHello() -> String {
     return "hi there"
 }
 
-struct Blah {
+public struct Blah {
     var ttt = "Hello blah"
 }
